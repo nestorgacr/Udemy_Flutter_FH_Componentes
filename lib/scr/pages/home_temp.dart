df7477d32580441589138f2bf1025e2c2da4e74a
Nestor Garcia <nestorgacr@gmail.com>
2020-04-10 17:17:19 -0600

    List view sin map y con map

--- lib/scr/pages/home_temp.dart
+++ lib/scr/pages/home_temp.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
-  const HomePageTemp({Key key}) : super(key: key);
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro'];
 
   @override
   Widget build(BuildContext context) {
@@ -9,17 +9,44 @@ class HomePageTemp extends StatelessWidget {
       appBar: AppBar(
         title: Text('Componentes Temp'),
       ),
-      body: ListView(
+      body: ListView(children: _crearItemsCorto()),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+
+    for (var opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista..add(tempWidget)..add(Divider());
+    }
+
+    return lista;
+  }
+
+//! Usando la propiedad map
+  List<Widget> _crearItemsCorto() {
+    var widgets = opciones.map((item) {
+      return Column(
         children: <Widget>[
           ListTile(
-            title: Text('ListTile Title'),
+            title: Text(item + ''),
+            subtitle: Text('Sub title'),
+            leading: Icon(
+              Icons.account_circle
+            ),
+            trailing: Icon(
+              Icons.arrow_back
+            ),
+            
           ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
+          Divider()
         ],
-      ),
-    );
+      );
+    }).toList();
+
+    return widgets;
   }
 }
