915acad073a28f065bf9048f418eefa1e5732d64
Nestor Garcia <nestorgacr@gmail.com>
2020-04-11 10:19:41 -0600

    Leer json, and futurebuilder

--- lib/scr/pages/home_page.dart
+++ lib/scr/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_componentes/scr/providers/menu_provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -12,19 +13,35 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    return ListView(
-      children: _listaItems(),
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [], //opcional
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(title: Text('Hola Mundo')),
-      Divider(),
-      ListTile(title: Text('Hola Mundo')),
-      Divider(),
-      ListTile(title: Text('Hola Mundo')),
-      Divider(),
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(Icons.account_circle),
+        trailing: Icon(Icons.arrow_right, color: Colors.blue,),
+        onTap: () {
+
+        },
+      );
+
+      opciones.add(widgetTemp);
+
+    });
+
+    return opciones;
+
   }
 }
