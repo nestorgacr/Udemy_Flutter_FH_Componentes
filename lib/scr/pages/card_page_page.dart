9c088f54c3aa0970761aba469132fd2862367b48
Nestor Garcia <nestorgacr@gmail.com>
2020-04-13 10:25:05 -0600

    cards

--- lib/scr/pages/card_page_page.dart
+++ lib/scr/pages/card_page_page.dart
@@ -1,13 +1,68 @@
 import 'package:flutter/material.dart';
 
 class CardPage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cards')
+      appBar: AppBar(title: Text('Cards')),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardTipo1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo2()
+        ],
+      ),
+    );
+  }
+
+  _cardTipo1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue),
+            title: Text('Soy el titulo de esta tarjeta'),
+            subtitle: Text(
+                'aqui estamos con una texto bien largo para ver como se muestra.'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {},
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  _cardTipo2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            placeholder: AssetImage('assets/cargando.gif'), 
+            fadeInDuration: Duration(milliseconds: 200 ),
+            image: NetworkImage('https://ticotimes.net/wp-content/uploads/2017/05/09-Guacalillo-Beach-360.jpg'),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('no tengo idea de que poner')
+          )
+        ],
       ),
     );
   }
